100a75c59f3d3e04143b6d42f597b28ee7531737
Obed <oe2126@columbia.edu>
2013-05-31 10:14:46 -0700

    one.c gives correct answer

--- one.c
+++ one.c
@@ -1,21 +1,51 @@
 /*
-  Find sum of multiples of
+  Find sum of multiples of two numbers that are less than a number.
 */
 
-#include <stdio.h>
-//#include <math.h>
+#include <stdio.h> // printf()
+#include <stdlib.h> // atoi()
 
-int multipleOf(int src);
+/* Functions */
+int multipleOf(int);
+void findSumOfMultiples(int, int, int);
+
+/* Resulting Sum */
 static int sumOfMultiples;
 
 int main (int argc, char **argv)
 {
-    if (argc != 2) {
-        
+    if (argc != 4) {
+        printf("USAGE: %s <multiple> <multiple> <lessThan>\n", argv[0]);
+        printf("EXAMPLE: %s 3 5 100\n", argv[0]);
+        return 1;
+    }
+
+    int multiple1 = atoi(argv[1]);
+    int multiple2 = atoi(argv[2]);
+    int lessThan = atoi(argv[3]);
+
+    findSumOfMultiples(multiple1, multiple2, lessThan);
+    
+    printf("sum of multiples of %d and %d below %d is %d\n",
+                multiple1, multiple2, lessThan, sumOfMultiples); 
 
     return 0;
 }
 
-int multipleOf(int number, int divisor) {
+int isMultipleOf(int number, int divisor) 
+{
     return number % divisor;
+}
+
+void findSumOfMultiples(int multiple1, int multiple2, int lessThan)
+{
+    int i = 0;
+
+    while (i < lessThan) {
+        if (!isMultipleOf(i, multiple1) || !isMultipleOf(i, multiple2)) {
+            sumOfMultiples += i;
+        } 
+        i++;
+    }
+
 }
