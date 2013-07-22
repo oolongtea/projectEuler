d891088e0add77a4a7dd9ac4cacea08141a37457
oolongtea <oe2126@columbia.edu>
2013-07-21 18:58:13 -0700

    added comments, prints correct usage, & prints only result

--- four.c
+++ four.c
@@ -11,34 +11,39 @@ int isPalindrome(char *str1, char *str2);
 
 int main(int argc, char **argv)
 {
-    // checks for correct usage
+    // check for correct usage
     if (argc != 2) {
-        printf("> This program yields the largest palindrome \n");
-        printf("> made from the product of two n-digit numbers.\n");
-        printf("USAGE: %s <numOfDigits>\n", argv[0]);
-        printf("EXAMPLE: %s 2\t\n", argv[0]);
+        printf("\tUSAGE ERROR\n"
+               "USAGE: %s <numOfDigits>\n"
+               "\n\tEXAMPLE\n"
+               "> Problem 4: Find the largest palindrome from the\n> "
+               "product of two 3-digit numbers.\n"
+               "COMMAND: %s 3\n"
+               "yields answer of 906609\n",  argv[0], argv[0]);
+        return 1;
     }
 
-
+    // store number of digits
     int digits = atoi(argv[1]);
 
+    // double for loop finds i * j, converts to char*, 
+    // compares with reverse string, and stores in 'palin' variable
+    // note: this takes a very long time
     int i, j, palin = 0;
     for (i = 1; numOfDigits(i) < (digits+1); i++) {
-    for (j = 1; numOfDigits(j) < (digits+1); j++) {
-    int num = i * j;
-    char *str = int2str(num);
-    char *rev = revStr(str);
-    if (isPalindrome(str, rev) == 0 && (num > palin)) {
-        palin = num;
-    }
-    //printf("num: %d\tstr: %s\trev: %s\n", num, str, rev);
-    //printf("palindrome: %d\n", isPalindrome(str, rev));
-    free(rev);
-    free(str); 
-    }
+        for (j = 1; numOfDigits(j) < (digits+1); j++) {
+            int num = i * j;
+            char *str = int2str(num);
+            char *rev = revStr(str);
+            if (isPalindrome(str, rev) == 0 && (num > palin)) {
+                palin = num;
+            }
+            free(rev);
+            free(str); 
+        }
     }
 
-
+    // print result
     printf("%d\n", palin);
     return 0;
 }
@@ -48,14 +53,14 @@ int numOfDigits(int num)
     return log10(num) + 1;
 }
 
+// integer to char*
 char *int2str(int num)
 {
-    //printf("numdig: %d\n", numOfDigits(num));
     int size = numOfDigits(num) + 1;
     char *str = (char *) malloc(size);
     memset(str, 0, size-1);
     sprintf(str, "%d", num);
-    //printf("%s\n", str);
+
     return str;
 }
 
@@ -67,6 +72,7 @@ char *revStr(char *num)
     memset(rev, 0, size);
     strcpy(rev, num);
 
+    // XOR method
     char *beg = rev;
     char *end = rev + strlen(rev) - 1;
     while (rev < end) {
